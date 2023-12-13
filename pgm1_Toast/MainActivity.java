9aa50bb13b801f3b3e09528ddf521620387d1e00
Tinu <117371766+TinuBose@users.noreply.github.com>
2023-12-13 22:42:11 +0530

    Add files via upload

---
+++ pgm1_Toast/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.pgm_1_toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toast.makeText(getApplicationContext(), "welcome to android", Toast.LENGTH_LONG).show();
+    }
+}
